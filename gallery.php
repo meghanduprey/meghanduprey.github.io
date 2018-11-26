cd20266b6b38be41f984a3fd57f575bc8684608b
meghan <meghan.duprey@gmail.com>
2018-11-26 06:44:11 -0500

    adding my existing site

---
+++ gallery.php
@@ -0,0 +1,66 @@
+<?php include 'header.php'; ?>
+       <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+                   <section class="flex-container">
+                <figure class="center">
+                    <img src= "assets/ss/diploma-compressor.jpg" alt= "Getting my diploma from the MMM Program at Syracuse University" style="height:220px">
+                    <figcaption>Getting my diploma from the MMM Program at Syracuse University</figcaption>
+                </figure>
+                <figure class="center">  
+                    <img src="assets/ss/meWIthACane-compressor.jpg" alt="My service dog, Taffy, and I at a Warrior Ride event" style="height:220px">
+                    <figcaption>My service dog, Taffy, and I at a Warrior Ride event</figcaption>
+                </figure>  
+                <figure class="center">
+                    <img src="assets/ss/meAndTaffy-compressor.jpg" alt="My dogs, Taffy and Nugget, and I at Camp Unleashed"  style="height:220px">
+                    <figcaption>My dogs, Taffy and Nugget, and I at Camp Unleashed</figcaption>
+                </figure>
+                <figure class="center">
+                    <img src="assets/ss/skydiving.jpg" alt="Skydiving!"  style="height:220px">
+                    <figcaption>Skydiving</figcaption>
+                </figure> 
+                <figure class="center">
+                    <img src="assets/ss/blues.jpg" alt="Me and my mom" style="height:220px">
+                    <figcaption>Me and my mom</figcaption>
+                </figure> 
+                <figure class="center">
+                    <img src="assets/ss/camphope.jpg" alt="Camp Hope Unleashed group picture" style="height:220px">
+                    <figcaption>Camp Hope Unleashed</figcaption>
+                </figure> 
+                <figure class="center">
+                    <img src="assets/ss/desert.jpg" alt="Deployed to Pakistan in 2003" style="height:220px">
+                    <figcaption>Deployed to Pakistan in 2003</figcaption>
+                </figure> 
+                <figure class="center">
+                    <img src="assets/ss/halloween.jpg" alt="Moose and Taffy in Halloween costumes" style="height:220px">
+                    <figcaption>We love Halloween</figcaption>
+                </figure> 
+                <figure class="center">
+                    <img src="assets/ss/helmet.jpg" alt="On a mission in Iraq" style="height:220px">
+                    <figcaption>On a mission in Iraq</figcaption>
+                </figure> 
+                <figure class="center">
+                    <img src="assets/ss/kayak2.jpg" alt="Kayaking with Taffy" style="height:220px">
+                    <figcaption>Kayaking with Taffy</figcaption>
+                </figure> 
+                <figure class="center">
+                    <img src= "assets/ss/movember.jpg" alt="Moose with a fake mustache on" style="height:220px">
+                    <figcaption>Movember with Moose</figcaption>
+                </figure> 
+                    <figure class="center">
+                <img src="assets/ss/nugget.jpg" alt="Nugget!" style="height:220px">
+                    <figcaption>Nugget!</figcaption>
+                </figure> 
+                <figure class="center">
+                    <img src="assets/ss/saudi.jpg" alt="Deployment to Saudi Arabia spring 2001" style="height:220px">
+                    <figcaption>Deployment to Saudi Arabia spring 2001</figcaption>
+                </figure> 
+                <figure class="center">
+                    <img src="assets/ss/statueofliberty.jpg" alt="On our way to see the Statue of Liberty!" style="height:220px">
+                    <figcaption>On our way to see the Statue of Liberty</figcaption>
+                </figure> 
+                <figure class="center">
+                    <img src="assets/ss/warriorride.jpg" alt="Me and Jess at a Warrior Ride event" style="height:220px">
+                    <figcaption>Me and Jess at a Warrior Ride event</figcaption>
+                </figure> 
+
+          </section>
+          <?php include 'footer.php'; ?>
